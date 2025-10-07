ec12a1341d5b3f7ebd3d2d9f0f24d581f17c1621
Kirill Turochkin <kirill180104@mail.ru>
2025-10-07 10:12:57 +0500

    lab8

---
+++ test-security2db-themeleaf/src/main/java/ru/itlearn/test_security2db_themeleaf/entity/Student.java
@@ -0,0 +1,33 @@
+package ru.itlearn.test_security2db_themeleaf.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "STUDENTS")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "faculty")
+    private String faculty;
+
+    @Column(name = "age")
+    private Integer age;
+}
